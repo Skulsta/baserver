2321a2d6d64f39c1ae5d65e87383461d04b506f0
skulsta <sti.sku@gmail.com>
2021-04-30 12:19:17 +0200

    Return filtered json data

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const { products } = require("./data");
 const app = express();
 
 app.listen(5000, () => {
   console.log("Listening on port 5000...");
+});
+
+app.get("/api/content", (req, res) => {
+  newProducts = products.map((product) => {
+    const { id, name } = product;
+    return { id, name };
+  });
+  res.json(newProducts);
 });
 
 app.use(express.static("./public"));
---
+++ data.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    id: "1",
+    name: "Cuz",
+    description: "Something clever",
+  },
+  {
+    id: "2",
+    name: "Sam",
+    description: "Another text here",
+  },
+];
+
+module.exports = { products };
