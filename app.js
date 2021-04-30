d957c2c417809a6bdfcce08da49ed2c6ae4b7f03
skulsta <sti.sku@gmail.com>
2021-04-30 15:57:11 +0200

    Removed commented code

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ app.listen(5000, () => {
   console.log("Listening on port 5000...");
 });
 
-// app.get("/api/content", (req, res) => {
-//   newProducts = products.map((product) => {
-//     const { id, name } = product;
-//     return { id, name };
-//   });
-//   res.json(newProducts);
-// });
-
 app.get("/api/content/:productId", (req, res) => {
   const { productId } = req.params;
   const singleProduct = products.find((product) => {
